02aad77bcdb47fbf8ddcba7c7e6b7fba765f6ac1
liamfallon <liam.fallon@est.tech>
2026-04-23 15:34:55 +0100

    Added gnerated upstream packages
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,6 +19,35 @@ rm boring-middleware/bm-subdir-b/Kptfile
 rm boring-middleware/bm-subdir-b/package-context.yaml
 rm boring-middleware/bm-subdir-b/starlark-set-to-dir-name.yaml
 
-yq -i '(del(.upstream)) | del(.upstreamLock) | del(.status)' boring-middleware/Kptfile
+yq -i 'del(.upstream, .upstreamLock, .status) | .metadata.annotations."kpt.dev/bfs-rendering" = "true"' boring-middleware/Kptfile
 
-#kpt fn render boring-middleware
+kpt fn render boring-middleware
+
+sed -i 's/boring-middleware/bm-subdir-a/g' boring-middleware/bm-subdir-a/*
+sed -i 's/boring-middleware/bm-subdir-b/g' boring-middleware/bm-subdir-b/*
+
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-subdir-a
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-subdir-b
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-1
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-1/wf-dep-subsubpkg-1-1
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-3
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-2
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-2/wf-dep-subsubpkg-2-1
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-2/wf-dep-subsubpkg-2-3
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-2/wf-dep-subsubpkg-2-2
+kpt pkg get https://github.com/liamfallon/upstream-packages/nginx-dir-named@nginx-dir-v1 wonderful-feature/wf-dep-subpkg-2/wf-dep-subsubpkg-2-2/wf-dep-subsubsubpkg-2-2-1
+
+rm wonderful-feature/wf-subdir-a/Kptfile
+rm wonderful-feature/wf-subdir-a/package-context.yaml
+rm wonderful-feature/wf-subdir-a/starlark-set-to-dir-name.yaml
+rm wonderful-feature/wf-subdir-b/Kptfile
+rm wonderful-feature/wf-subdir-b/package-context.yaml
+rm wonderful-feature/wf-subdir-b/starlark-set-to-dir-name.yaml
+
+yq -i 'del(.upstream, .upstreamLock, .status) | .metadata.annotations."kpt.dev/bfs-rendering" = "true"' wonderful-feature/Kptfile
+
+kpt fn render wonderful-feature
+
+sed -i 's/wonderful-feature/wf-subdir-a/g' wonderful-feature/wf-subdir-a/*
+sed -i 's/wonderful-feature/wf-subdir-b/g' wonderful-feature/wf-subdir-b/*
